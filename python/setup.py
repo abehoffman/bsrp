ec4643ed61456fd1d3240205f4d33b261a088ece
Abe Hoffman <53541863+abehoffman@users.noreply.github.com>
2021-03-21 18:40:37 -0600

    additional setup

--- python/setup.py
+++ python/setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Topic :: Security',
     ],
     python_requires='>=3.5',
 )
